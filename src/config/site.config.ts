5e7c91de5e5c25786e260209ab3eb0b788326783
sachigoyal <sachigoyal27@gmail.com>
2026-02-01 22:10:46 +0600

    feat: site config for metadata

---
+++ src/config/site.config.ts
@@ -0,0 +1,30 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "sachi.dev",
+  title: "Sachi | Software Developer | Hi 👋 I'm Sachi Goyal, a software developer who loves to make pretty web experiences.",
+  description: "Hi 👋 I'm Sachi Goyal, a software developer who loves to make pretty web experiences.",
+  origin: "https://sachi.dev",
+  keywords: [
+    "sachi",
+    "sachii",
+    "sachi goyal",
+    "Sachi Goyal",
+    "sachi.dev",
+    "software developer",
+    "web developer",
+    "full stack developer",
+    "frontend developer",
+  ],
+  og: "https://sachi.dev/og.png",
+  creator: {
+    name: "sachigoyal",
+    url: "https://sachi.dev",
+  },
+  socials: {
+    github: "https://github.com/sachigoyal",
+    x: "https://x.com/sachigoyal27",
+    linkedin: "https://www.linkedin.com/in/sachi-goyal-921327250/",
+    buymeacoffee: "https://www.buymeacoffee.com/sachigoyalk",
+  }
+}
